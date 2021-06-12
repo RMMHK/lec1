659d663d824da5267e4f793d42c6fb5863cc4cdc
RMMHK <rmmhkr@gmail.com>
2021-06-12 19:41:50 +0500

    KK

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <h1>hello</h1>
-          <h1>hello</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrersss"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <a href ="https://www.facebook.com">FaceBook</a>
+      <img src ="1.png"></img>
+   <h1> Rana</h1>
+   <h2>Mustafa</h2>
+   </div> 
   );
 }
 
